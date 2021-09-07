b6968c1e4eca858bb0b3f1abb38af5e846099ffb
Spyro1001 <30807727+Spyro1001@users.noreply.github.com>
2021-09-07 09:29:57 -0700

    Initial commit

---
+++ src/main/java/com/csc205/project2/Project2.java
@@ -0,0 +1,41 @@
+package com.csc205.project2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Driver for project 2.
+ *
+ * Modify the driver as needed to demonstrate your classes.
+ *
+ */
+public class Project2 {
+
+    public static void main(String[] args) {
+
+        /*
+        The following code works with the example design given.
+         */
+
+        /*ThreeDimensionalShape sphere = new Sphere(2.0);
+        ThreeDimensionalShape cube = new Cube(5.0);
+        ThreeDimensionalShape cylinder = new Cylinder(4.0, 1.0);
+
+        // We'll cover this stuff in module 4
+        List<ThreeDimensionalShape> shapes = new ArrayList<>();
+        shapes.add(sphere);
+        shapes.add(cube);
+        shapes.add(cylinder);
+
+        shapes.forEach(System.out::println);*/
+
+        /*
+        The following code works with the code given.
+         */
+
+        Sphere sphere = new Sphere(2.0);
+        System.out.println(sphere);
+    }
+
+}
+
